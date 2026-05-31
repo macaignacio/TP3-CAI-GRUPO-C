f7571c7d88988c5675ea565f68c259847ba64688
DylanFeld <91FE41470609@campus.economicas.uba.ar>
2026-05-31 20:33:48 -0300

    CtaCteFleteros y CtaCteEmpresaOmnibus. Almacen y entidad

--- TP3 CAI GRUPO C/Almacenes/CuentaCorrienteEmpresaOmnibusAlmacen.cs
+++ TP3 CAI GRUPO C/Almacenes/CuentaCorrienteEmpresaOmnibusAlmacen.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace TP3_CAI_GRUPO_C.Almacenes
 {
     internal class CuentaCorrienteEmpresaOmnibusAlmacen
     {
+        public static List<CuentaCorrienteEmpresaOmnibusEntidad> ctaCteEmpresaOmnibus = new();
+
+        static CuentaCorrienteEmpresaOmnibusAlmacen()
+        {
+            if (File.Exists(@"datos\CtaCteEmpresaOmnibus.json"))
+            {
+                string json = File.ReadAllText(@"datos\CtaCteEmpresaOmnibus.json");
+                ctaCteEmpresaOmnibus = JsonSerializer.Deserialize<List<CuentaCorrienteEmpresaOmnibusEntidad>>(json);
+            }
+        }
+
+        public static void Guardar()
+        {
+            string json = JsonSerializer.Serialize(ctaCteEmpresaOmnibus);
+            File.WriteAllText(@"datos\CtaCteEmpresaOmnibus.json", json);
+        }
+
     }
 }
--- TP3 CAI GRUPO C/Almacenes/CuentaCorrienteEmpresaOmnibusEntidad.cs
+++ TP3 CAI GRUPO C/Almacenes/CuentaCorrienteEmpresaOmnibusEntidad.cs
@@ -6,5 +6,12 @@ namespace TP3_CAI_GRUPO_C.Almacenes
 {
     internal class CuentaCorrienteEmpresaOmnibusEntidad
     {
+        public long CuitEmpresaOmnibus { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Comprobante { get; set; }
+        public decimal Importe { get; set; }
+        public decimal Debe { get; set; }
+        public decimal Haber { get; set; }
+        public decimal Saldo { get; set; }
     }
 }
