39618595e63558afbf515d48703c57cbc65bdf2c
MACARENA ABRIL IGNACIO <90IG42496828@campus.economicas.uba.ar>
2026-05-31 19:04:16 -0300

    creación de clases dentro de carpeta Almacenes

---
+++ TP3 CAI GRUPO C/Almacenes/ClienteEntidad.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP3_CAI_GRUPO_C.Almacenes
+{
+    internal class ClienteEntidad
+    {
+    }
+}
---
+++ TP3 CAI GRUPO C/Almacenes/MetodoEntregaEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP3_CAI_GRUPO_C.Almacenes
+{
+    public enum MetodoEntregaEnum
+    {
+     ADomicilio,
+     CentroDeDistribucion,
+     Agencia,
+        
+
+    }
+}
