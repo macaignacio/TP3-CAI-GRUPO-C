ce76b56a7bf25d0b5fc6dd4b1b2f9ab0d718470f
Martin <martiale2002@gmail.com>
2026-05-31 20:21:37 -0300

    creacion de provincia entidad y almacen, creacion de localidad entidad y almacen (ambos con sus archivos json) + creación de clase auxiliar de ParadaOmnibus

--- TP3 CAI GRUPO C/Almacenes/ProvinciaEntidad.cs
+++ TP3 CAI GRUPO C/Almacenes/ProvinciaEntidad.cs
@@ -6,5 +6,10 @@ namespace TP3_CAI_GRUPO_C.Almacenes
 {
     internal class ProvinciaEntidad
     {
+        public int idProvincia { get; set; }
+
+        public string nombre { get; set; }
+
+        public List<int> Localidades { get; set; }
     }
 }
