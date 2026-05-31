9bd676c8ff940aaa9f1866755f3796b60e1167b2
MACARENA ABRIL IGNACIO <90IG42496828@campus.economicas.uba.ar>
2026-05-31 20:04:02 -0300

    ctacte cliente y agencia

--- TP3 CAI GRUPO C/Almacenes/CuentaCorrienteAgenciaEntidad.cs
+++ TP3 CAI GRUPO C/Almacenes/CuentaCorrienteAgenciaEntidad.cs
@@ -6,5 +6,14 @@ namespace TP3_CAI_GRUPO_C.Almacenes
 {
     internal class CuentaCorrienteAgenciaEntidad
     {
+        public string CodigoAgencia { get; set; }
+        public DateTime Fecha { get; set; }
+        public long NumeroGuia { get; set; }
+        public string Comprobante { get; set; }
+        public decimal Comision { get; set; }
+        public decimal Debe { get; set; }
+        public decimal Haber { get; set; }
+        public decimal Saldo { get; set; }
+
     }
 }
