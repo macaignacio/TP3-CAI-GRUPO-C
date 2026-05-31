4bfed70a5950f91d2b55590fe0f66ca6132e15de
MACARENA ABRIL IGNACIO <90IG42496828@campus.economicas.uba.ar>
2026-05-31 20:19:42 -0300

    facturas

--- TP3 CAI GRUPO C/Almacenes/FacturaAlmacen.cs
+++ TP3 CAI GRUPO C/Almacenes/FacturaAlmacen.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
+using TP3_CAI_GRUPO_C.FacturarCliente;
 
 namespace TP3_CAI_GRUPO_C.Almacenes
 {
     internal class FacturaAlmacen
-    {
+    {public static List<FacturaEntidad> facturas = new();
+
+        static FacturaAlmacen()
+        {
+            if (File.Exists(@"datos\facturas.json"))
+            {
+                string json = File.ReadAllText(@"datos\facturas.json");
+                facturas = JsonSerializer.Deserialize<List<FacturaEntidad>>(json);
+            }
+
+        }
+
+        public static void Guardar()
+
+        {
+            string json = JsonSerializer.Serialize(facturas);
+            File.WriteAllText(@"datos\facturas.json", json);
+        }
+
     }
 }
--- TP3 CAI GRUPO C/Almacenes/FacturaEntidad.cs
+++ TP3 CAI GRUPO C/Almacenes/FacturaEntidad.cs
@@ -5,6 +5,18 @@ using System.Text;
 namespace TP3_CAI_GRUPO_C.Almacenes
 {
     internal class FacturaEntidad
-    {
+    {   //datos cabecera
+        public string NumeroFactura { get; set; }
+        public long CuitCliente { get; set; }
+        public DateTime FechaEmision { get; set; }
+        public int Total { get; set; }
+        public DateTime Vencimiento { get; set; }
+
+
+        //movimientos
+        public List<long> Guias { get; set; }
+
     }
+
+}
 }
