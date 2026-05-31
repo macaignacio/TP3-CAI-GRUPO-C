0c07adfeefe00afd21aef5a42bc38f1079d7ee89
MACARENA ABRIL IGNACIO <90IG42496828@campus.economicas.uba.ar>
2026-05-31 20:10:09 -0300

    cliente

--- TP3 CAI GRUPO C/Almacenes/ClienteAlmacen.cs
+++ TP3 CAI GRUPO C/Almacenes/ClienteAlmacen.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace TP3_CAI_GRUPO_C.Almacenes
 {
     internal class ClienteAlmacen
     {
+        public static List<ClienteEntidad> clientes = new();
+
+        static ClienteAlmacen()
+        {
+            if (File.Exists(@"datos\clientes.json"))
+            {
+                string json = File.ReadAllText(@"datos\clientes.json");
+                clientes = JsonSerializer.Deserialize<List<ClienteEntidad>>(json);
+            }
+        }
+
+        public static void Guardar()
+        {
+            string json = JsonSerializer.Serialize(clientes);
+            File.WriteAllText(@"datos\clientes.json", json);
+        }
+
     }
 }
--- TP3 CAI GRUPO C/Almacenes/ClienteEntidad.cs
+++ TP3 CAI GRUPO C/Almacenes/ClienteEntidad.cs
@@ -6,5 +6,8 @@ namespace TP3_CAI_GRUPO_C.Almacenes
 {
     internal class ClienteEntidad
     {
+        public long Cuit { get; set; }
+        public string RazonSocial { get; set; }
+        public string CondicionIva { get; set; }
     }
 }
