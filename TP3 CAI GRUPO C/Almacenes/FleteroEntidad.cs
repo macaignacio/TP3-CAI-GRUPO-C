acc24ab66f8b553b0d7c61fdfce796dee6b46649
MACARENA ABRIL IGNACIO <90IG42496828@campus.economicas.uba.ar>
2026-05-31 21:02:01 -0300

    entidades precios

--- TP3 CAI GRUPO C/Almacenes/FleteroEntidad.cs
+++ TP3 CAI GRUPO C/Almacenes/FleteroEntidad.cs
@@ -6,16 +6,16 @@ namespace TP3_CAI_GRUPO_C.Almacenes
 {
     internal class FleteroEntidad
     {
-        public long cuitCuilFletero { get; set; }
+        public long CuitCuilFletero { get; set; }
 
-        public string nombreCompleto { get; set; }
+        public string NombreCompleto { get; set; }
 
-        public decimal comisionS { get; set; }
+        public decimal ComisionS { get; set; }
 
-        public decimal comisionM { get; set; }
+        public decimal ComisionM { get; set; }
 
-        public decimal comisionL { get; set; }
+        public decimal ComisionL { get; set; }
 
-        public decimal comisionXL { get; set; }
+        public decimal ComisionXL { get; set; }
     }
 }
