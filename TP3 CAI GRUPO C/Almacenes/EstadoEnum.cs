ad30d6eb57d4e039d585d7ee5bebcde13b5f9b64
MACARENA ABRIL IGNACIO <90IG42496828@campus.economicas.uba.ar>
2026-05-31 22:17:17 -0300

    comentario enum

--- TP3 CAI GRUPO C/Almacenes/EstadoEnum.cs
+++ TP3 CAI GRUPO C/Almacenes/EstadoEnum.cs
@@ -6,18 +6,18 @@ namespace TP3_CAI_GRUPO_C.Almacenes
 {
     public enum EstadoEnum
     {
-        ImpuestaTelefonicamente,
-        ImpuestaEnAgencia,
-        ImpuestaEnCD,
-        RetiroDomicilioEnCurso,
-        RetiroAgenciaEnCurso,
-        AdmitidaEnCD,
-        EnTransitoACDDestino,
-        EnCDDestino,
-        ListaParaEntregarPorCD,
-        EnTransitoEntregaDomicilio,
-        EnTransitoAAgenciaDestino,
-        ListaParaEntregarPorAgencia,
-        Entregado
+        ImpuestaTelefonicamente, //1
+        ImpuestaEnAgencia,//2
+        ImpuestaEnCD,//3
+        RetiroDomicilioEnCurso,//4
+        RetiroAgenciaEnCurso,//5
+        AdmitidaEnCD,//6
+        EnTransitoACDDestino,//7
+        EnCDDestino,//8
+        ListaParaEntregarPorCD,//9
+        EnTransitoEntregaDomicilio,//10
+        EnTransitoAAgenciaDestino,//11
+        ListaParaEntregarPorAgencia,//12
+        Entregado//13
     }
 }
