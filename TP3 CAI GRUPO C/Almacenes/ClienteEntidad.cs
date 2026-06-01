4eb21c45741163d3b4ac374fe8075bb7f78d0595
Facundo Maggio <facundomaggio00@hotmail.com>
2026-05-31 22:18:17 -0300

    json

--- TP3 CAI GRUPO C/Almacenes/ClienteEntidad.cs
+++ TP3 CAI GRUPO C/Almacenes/ClienteEntidad.cs
@@ -9,5 +9,6 @@ namespace TP3_CAI_GRUPO_C.Almacenes
         public long Cuit { get; set; }
         public string RazonSocial { get; set; }
         public string CondicionIva { get; set; }
+
     }
 }
