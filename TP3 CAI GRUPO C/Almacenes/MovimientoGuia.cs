a431c85577a18ea24ee2ab80745039da5938d8c6
Facundo Maggio <facundomaggio00@hotmail.com>
2026-05-31 21:03:12 -0300

    Commit MovimientoGuia

--- TP3 CAI GRUPO C/Almacenes/MovimientoGuia.cs
+++ TP3 CAI GRUPO C/Almacenes/MovimientoGuia.cs
@@ -6,5 +6,9 @@ namespace TP3_CAI_GRUPO_C.Almacenes
 {
     internal class MovimientoGuia
     {
+        public DateTime FechaHoraPara { get; set; }
+
+        public string CentroDistribucionParada { get; set; }
+
     }
 }
