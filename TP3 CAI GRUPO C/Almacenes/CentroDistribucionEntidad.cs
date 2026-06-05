8b387d5e30c58692f67387d2de87031d6fe75c73
MACARENA ABRIL IGNACIO <90IG42496828@campus.economicas.uba.ar>
2026-06-05 16:37:46 -0300

    direc y horarios agencias y cd

--- TP3 CAI GRUPO C/Almacenes/CentroDistribucionEntidad.cs
+++ TP3 CAI GRUPO C/Almacenes/CentroDistribucionEntidad.cs
@@ -7,12 +7,11 @@ namespace TP3_CAI_GRUPO_C.Almacenes
     internal class CentroDistribucionEntidad
     {
         public string Codigo { get; set; }
-
         public string Nombre { get; set; }
-
         public int IdLocalidad { get; set; }
-
         public List<int> CodPostal { get; set; }
+        public string Direccion { get; set; }
+        public string Horarios { get; set; }
         public List<long> Fleteros { get; set; }
 
     }
