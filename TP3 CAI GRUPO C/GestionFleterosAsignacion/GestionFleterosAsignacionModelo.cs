ca5de87b0b85af7b610ba3ea1fc796b47ba3b4cc
Martin <martiale2002@gmail.com>
2026-06-09 18:38:45 -0300

    arreglo de estados en el modelo por la creacion del nuevo estado en EstadoEnum

--- TP3 CAI GRUPO C/GestionFleterosAsignacion/GestionFleterosAsignacionModelo.cs
+++ TP3 CAI GRUPO C/GestionFleterosAsignacion/GestionFleterosAsignacionModelo.cs
@@ -115,13 +115,13 @@ namespace TP3_CAI_GRUPO_C.GestionFleterosAsignacion
             if ((int)guia.EstadoActual == 1)
                 return (EstadoEnum)4;
 
-            if ((int)guia.EstadoActual == 7
+            if ((int)guia.EstadoActual == 8
                 && (int)guia.MetodoEntrega == 0)
-                return (EstadoEnum)9;
-
-            if ((int)guia.EstadoActual == 7
-                && (int)guia.MetodoEntrega == 2)
                 return (EstadoEnum)10;
+
+            if ((int)guia.EstadoActual == 8
+                && (int)guia.MetodoEntrega == 2)
+                return (EstadoEnum)11;
 
             return guia.EstadoActual;
         }
